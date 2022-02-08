a2813c4bf007d0acbda8417103c1a26a529fad50
imran <imranmr1997@gmail.com>
2022-02-08 11:19:05 +0530

    reuploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"c",component:CreateaccountComponent},
+  {path:"l",component:LoginComponent},
+  {path:"a",component:AddEmployeeComponent},
+  {path:"e",component:EditEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateaccountComponent } from './createaccount/createaccount.component';
+import { LoginComponent } from './login/login.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateaccountComponent,
+    LoginComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
       
   ],
   imports: [
